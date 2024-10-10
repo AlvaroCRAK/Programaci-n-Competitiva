8c61e30d2885ac1193b10cd0535355aa3a77a79c
Alvaro zublime <131735140+AlvaroCRAK@users.noreply.github.com>
2024-10-10 17:05:02 -0400

    Update 8699. Capitalize first four characters.java

--- OmegaUp/8699. Capitalize first four characters.java
+++ OmegaUp/8699. Capitalize first four characters.java
@@ -12,3 +12,21 @@ public class Main {
     System.out.println(name);
   }
 }
+
+/* 
+import java.util.Scanner;
+public class Main {
+  public static void main ( String[] args ) {
+    Scanner scanner = new Scanner ( System.in );
+    String name = scanner.next();
+    char[] nameChar = name.toCharArray();
+    for ( int i = 0; i < nameChar.length; i++ ) {
+      char c = nameChar[i];
+      if ( c >= 'a' && c <= 'z' )
+        nameChar[i] -= 32;
+      if ( i == 3 ) break;
+    }
+    System.out.println ( new String ( nameChar ) );
+  }
+}
+*/
