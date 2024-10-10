cb9ce6fe83bb53d2ce9676a7fda80a754a97207b
Alvaro zublime <131735140+AlvaroCRAK@users.noreply.github.com>
2024-10-10 17:16:38 -0400

    Create 6205. ABCdario.java

---
+++ OmegaUp/6205. ABCdario.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+  static String func ( String alphabet ) {
+    boolean[] letters = new boolean [26];
+    Arrays.fill ( letters, false );
+    for ( int i = 0; i < alphabet.length(); i++ ) {
+      int n = (int) (alphabet.charAt(i) - 'a');
+      letters[n] = true;
+    }
+    for ( int i = 0; i < letters.length; i++ ) 
+      if ( letters[i] == false )
+        return "NO WAY";
+    return "PERFECT";
+  }
+  public static void main ( String[] args ) {
+    Scanner scanner = new Scanner ( System.in );
+    int n = scanner.nextInt();
+    while ( n-- > 0 ) {
+      String alphabet = scanner.next();
+      System.out.println ( func(alphabet) );
+    }
+  }
+}
