63e0249322c5dc5dddb61c8e086d2d9044c38ffa
Alvaro zublime <131735140+AlvaroCRAK@users.noreply.github.com>
2024-10-17 19:47:56 -0400

    Create 242. Valid Anagram.cpp

---
+++ LeetCode/242. Valid Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+  unordered_map<char, int> map;
+  for ( int i = 0; i < s.length(); i++ ) 
+    map[s[i]] ++;
+  for ( int i = 0; i < t.length(); i++ ) 
+    map[t[i]] --;
+
+  for ( auto i : map ) 
+    if ( i.second != 0 )
+      return false;
+  return true;
+        
+    }
+};
