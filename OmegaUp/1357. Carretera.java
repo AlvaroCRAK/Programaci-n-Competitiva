02ab2e451fac1ac224c6d7da78e66108967efd8c
Alvaro zublime <131735140+AlvaroCRAK@users.noreply.github.com>
2024-11-21 18:18:31 -0500

    Update 1357. Carretera.java

--- OmegaUp/1357. Carretera.java
+++ OmegaUp/1357. Carretera.java
@@ -1,52 +1,50 @@
-import java.util.Scanner;
-import java.util.HashMap;
-public class Main {
-  static class Car {
-    int x;
-    int v;
-    Car ( int x, int v ) {
-      this.x = x;
-      this.v = v;
-    }
-  }
-  static double distance ( HashMap<Integer, Car> cars, double time ) {
-    double position_max = -1e5;
-    double position_min = 1e5;
-    for ( int i : cars.keySet() ) {
-      double position_car = (double) cars.get(i).x + cars.get(i).v * time;
-      position_max = Math.max ( position_max, position_car );
-      position_min = Math.min ( position_min, position_car );
-    }
-    return position_max - position_min;
-  }
-  static double func ( HashMap<Integer, Car> cars ) {
-    final double EPSILON = 1e-7;
-    double hight = 1e5;
-    double low = 0;
-    while ( hight - low >= EPSILON ) {
-      double time1 = low + ( hight - low ) / 3;
-      double time2 = hight - ( hight - low ) / 3;
-      
-      double distance_low = distance ( cars, time1 );
-      double distance_hight = distance ( cars, time2 );
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-      if ( distance_low < distance_hight )
-        hight = time2;
-      else 
-        low = time1;
+public class Main {
+    static double dis(int[][] cars, double time) {
+        double max = -1e5;
+        double min = 1e5;
+        for (int i = 0; i < cars[0].length; i++) {
+            double pos = (double)cars[0][i] + cars[1][i] * time;
+            max = Math.max(max, pos);
+            min = Math.min(min, pos);
+        }
+        return max - min;
     }
-    return ( distance ( cars, low ) );
-  }
-  public static void main ( String[] args ) {
-    Scanner scanner = new Scanner ( System.in );
-    int n = scanner.nextInt();
-    HashMap<Integer, Car> cars = new HashMap<>();
-    while ( n-- > 0 ) {
-      int x = scanner.nextInt();
-      int v = scanner.nextInt();
-      Car car = new Car ( x, v );
-      cars.put ( n, car );
+
+    static double func(int[][] cars) {
+        double low = 0;
+        double high = 1e5;
+        final double EPSILON = 1e-6;
+        while (high - low >= EPSILON) {
+            double mid1 = low + (high - low) / 3;
+            double mid2 = high - (high - low) / 3;
+
+            double distance1 = dis(cars, mid1);
+            double distance2 = dis(cars, mid2);
+
+            if (distance1 < distance2)
+                high = mid2;
+            else
+                low = mid1;
+        }
+        return dis(cars, low);
     }
-    System.out.printf ( "%f", func ( cars ) );
-  }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] firstLine = br.readLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int[][] cars = new int[2][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] carData = br.readLine().split(" ");
+            cars[0][i] = Integer.parseInt(carData[0]);  // x position
+            cars[1][i] = Integer.parseInt(carData[1]);  // velocity
+        }
+
+        System.out.printf("%.6f", func(cars));
+    }
 }
